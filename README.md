a419d47a69cdd2e1b2e6c15f111b5832c878b455
kumudug <kumudug1@gmail.com>
2021-09-04 06:22:53 -0700

    Add the backend load balancer url

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
    - `docker-compose up -d --build`
 * Stop
    - `docker-compose down -v`
+
+* Build prod version of frontend
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import GoalInput from './components/goals/GoalInput';
 import CourseGoals from './components/goals/CourseGoals';
 import ErrorAlert from './components/UI/ErrorAlert';
 
+//process.env.NODE_ENV is set in react build process
+const backendUrl = process.env.NODE_ENV === 'development' ? 'http://localhost' : 'http://fargate-test-522996165.us-west-2.elb.amazonaws.com'
+
 function App() {
   const [loadedGoals, setLoadedGoals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -14,7 +17,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch(backendUrl + '/goals');
 
         const resData = await response.json();
 
@@ -39,7 +42,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch(backendUrl + '/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +81,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch(backendUrl + '/goals/' + goalId, {
         method: 'DELETE',
       });
 
